533facd364a5f7ce1180031d5978078189c2bc43
ItsZii <95366771+ItsZii@users.noreply.github.com>
2021-12-09 18:55:23 +0200

    Add files via upload

---
+++ new-prakt3/App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import HomeScreen from './components/HomeScreen';
+
+import SecondScreen from './components/ListView';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }}/>
+        <Stack.Screen name="ListView" component={SecondScreen} options={{ title: 'List' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
